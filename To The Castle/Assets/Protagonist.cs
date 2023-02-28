17375d96c151a22a8528680b76995534aa87634f
MEMaicoSt <mary.steele@my.utsa.edu>
2023-02-28 16:27:38 -0600

    adding movement
    I gave my protagonist the ability to move. I still need to create a sprite sheet for her, and I also need to find a way to make her unable to fly off into the blue screen behind the level background.

---
+++ To The Castle/Assets/Protagonist.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protagonist : MonoBehaviour
+{
+    
+    Rigidbody2D pRB;
+
+    float runSpeed = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        pRB.velocity = (new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))* runSpeed);
+    }
+}
